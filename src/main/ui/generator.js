05b9cd30f0cee07b6195b50b4f0ae1784cc692e7
Andrew <dronx.wow@gmail.com>
2015-08-18 22:33:13 +0300

    rewrite generator

--- src/main/ui/generator.js
+++ src/main/ui/generator.js
@@ -3,85 +3,42 @@ import * as CELLS from './megacells'
 var megacells = CELLS.megacells;
 
 export function generateCells(T) {
-    let cells = [];
+	let cells = [];
+	const cell_max = parseInt(Math.pow(4, T));
+	const side_max = parseInt(Math.pow(2, T)) - 1;
 
-    for ( let i = 0; i < 20 * parseInt(Math.pow(4, T)); ++i )
-        cells.push({
-            state: false,
-            links: []
-        });
+	let edge_cells = [[],[],[]]; // L B R
+	for(let x = 0; x <= side_max;++x){
+		edge_cells[0].push(x*2);
+		edge_cells[1].push(cell_max - 1 - parseInt(Math.pow(side_max - x, 2)));
+		edge_cells[2].push(cell_max - 1 - x * (x + 2));
+	}
 
-    let c = 0;
-    const cell_max = parseInt(Math.pow(4, T));
-    const side_max = parseInt(Math.pow(2, T)) - 1;
+	for ( let i = 0; i < 20 * cell_max; ++i )
+		cells.push({
+			state: false,
+			links: []
+		});
 
-    for ( let M = 0; M < 20; ++M ) { // M for megacell
-        for ( let t = 0; t < parseInt(Math.pow(2, T)); ++t ) { // "row"
-            let r = parseInt(Math.pow(2, T + 1)) - (1 + t) * 2; // number of elements in the row
-            for ( let n = 0; n <= r; ++n ) { // position within a row
-                if ( n % 2 ) {
-                    cells[c].links.push(c - 1);
-                    cells[c].links.push(c + 1);
-                    cells[c].links.push(c + r);
-                }
-                else {
-                    if ( n != 0 ) cells[c].links.push(c - 1);
-                    if ( n != r ) cells[c].links.push(c + 1);
-                    if ( t != 0 )cells[c].links.push(c - (r + 2));
-                }
-                c++;
-            }
-        }
+	for ( let M = 0; M < 20; ++M ) { // M for megacell
+		for ( let t = 0; t <= side_max; ++t ) { // "row"
+			let r = 2*(side_max-t); // max element in the row
+			let c = cell_max*(M+1)-parseInt(Math.pow(side_max-t+1,2));
+			for ( let n = 1; n <= r; n+=2 ) { // position within a row
+				let y = c + n;
+				cells[y].links.push(y - 1);
+				cells[y].links.push(y + 1);
+				cells[y].links.push(y + r);
+				for(let d of cells[y].links) cells[d].links.push(y);
+			}
+		}
 
-        switch ( megacells[M].rL ) {
-            case 0:
-                for ( let i = 0; i <= side_max; ++i )
-                    cells[i * 2 + cell_max * M].links.push((side_max - i) * 2 + cell_max * megacells[M].L);
-                break;
-            case 1:
-                for ( let i = 0; i <= side_max; ++i )
-                    cells[i * 2 + cell_max * M].links.push(parseInt(Math.pow(4, T)) - 1 - parseInt(Math.pow(parseInt(Math.pow(2, T)) - (side_max - i) - 1, 2)) + cell_max * megacells[M].L);
-                break;
-            case 2:
-                for ( let i = 0; i <= side_max; ++i )
-                    cells[i * 2 + cell_max * M].links.push(parseInt(Math.pow(4, T)) - 1 - (side_max - i) * ((side_max - i) + 2) + cell_max * megacells[M].L);
-                break;
-        }
+		for ( let i = 0,ii = side_max; i <= side_max; ++i,--ii ){
+			cells[edge_cells[0][i] + cell_max * M].links.push(edge_cells[megacells[M].rL][ii]+ cell_max * megacells[M].L);
+			cells[edge_cells[1][i] + cell_max * M].links.push(edge_cells[megacells[M].rB][ii]+ cell_max * megacells[M].B);
+			cells[edge_cells[2][i] + cell_max * M].links.push(edge_cells[megacells[M].rR][ii]+ cell_max * megacells[M].R);
+		}
+	}
 
-        switch ( megacells[M].rB ) {
-            case 0:
-                for ( let i = 0; i <= side_max; ++i )
-                    cells[parseInt(Math.pow(4, T)) - 1 - parseInt(Math.pow(parseInt(Math.pow(2, T)) - i - 1, 2)) + cell_max * M].links.push((side_max - i) * 2 + cell_max * megacells[M].B);
-                break;
-            case 1:
-                for ( let i = 0; i <= side_max; ++i )
-                    cells[parseInt(Math.pow(4, T)) - 1 - parseInt(Math.pow(parseInt(Math.pow(2, T)) - i - 1, 2)) + cell_max * M].links.push(parseInt(Math.pow(4,
-                            T)) - 1 - parseInt(Math.pow(parseInt(Math.pow(2, T)) - (side_max - i) - 1, 2)) + cell_max * megacells[M].B);
-                break;
-            case 2:
-                for ( let i = 0; i <= side_max; ++i )
-                    cells[parseInt(Math.pow(4, T)) - 1 - parseInt(Math.pow(parseInt(Math.pow(2, T)) - i - 1, 2)) + cell_max * M].links.push(parseInt(Math.pow(4,
-                            T)) - 1 - (side_max - i) * ((side_max - i) + 2) + cell_max * megacells[M].B);
-                break;
-        }
-
-        switch ( megacells[M].rR ) {
-            case 0:
-                for ( let i = 0; i <= side_max; ++i )
-                    cells[parseInt(Math.pow(4, T)) - 1 - i * (i + 2) + cell_max * M].links.push((side_max - i) * 2 + cell_max * megacells[M].R);
-                break;
-            case 1:
-                for ( let i = 0; i <= side_max; ++i )
-                    cells[parseInt(Math.pow(4, T)) - 1 - i * (i + 2) + cell_max * M].links.push(parseInt(Math.pow(4, T)) - 1 - parseInt(Math.pow(parseInt(Math.pow(2,
-                                T)) - (side_max - i) - 1, 2)) + cell_max * megacells[M].R);
-                break;
-            case 2:
-                for ( let i = 0; i <= side_max; ++i )
-                    cells[parseInt(Math.pow(4, T)) - 1 - i * (i + 2) + cell_max * M].links.push(parseInt(Math.pow(4,
-                            T)) - 1 - (side_max - i) * ((side_max - i) + 2) + cell_max * megacells[M].R);
-                break;
-        }
-    }
-
-    return cells;
+	return cells;
 }
