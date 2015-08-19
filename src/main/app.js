d274d6d5c1d15601a1b896c23186c133e81164b4
orion <crissorion@gmail.com>
2015-08-19 09:55:04 +0300

    ui functions refactored to class

--- src/main/app.js
+++ src/main/app.js
@@ -1,6 +1,9 @@
 import Game from './engine/game'
 import WorldMapFactory from './engine/worldmapfactory'
-import * as UI from './ui/main'
+import Sphere from './ui/main'
 
-UI.init();
-UI.animate();
+let container = document.createElement('div');
+document.body.appendChild(container);
+
+var sphere = new Sphere(container);
+sphere.animate();
--- src/main/ui/main.js
+++ src/main/ui/main.js
@@ -6,62 +6,75 @@ let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 let ind = 0;
 let sph;
-let T = 2;
-let cells = CellGenerator.generateCells(T);
 
-document.addEventListener('mousemove', onDocumentMouseMove, false);
+
 
 function onDocumentMouseMove(event) {
     mouseX = ( event.clientX - windowHalfX ) / windowHalfX;
     mouseY = ( event.clientY - windowHalfY ) / windowHalfY;
 }
 
-export function init() {
-    let container = document.createElement('div');
-    document.body.appendChild(container);
+module.exports = class Sphere {
+    constructor(container, game) {
+        this.container = container;
+        this.game = game;
+        this.detalisationLevel = 0;
+        this.cells = CellGenerator.generateCells(this.detalisationLevel);
+        this.init();
+    }
 
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 10000);
+    init() {
+        document.addEventListener('mousemove', onDocumentMouseMove, false);
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 10000);
 
-    scene = new THREE.Scene();
-    sph = new THREE.IcosahedronGeometry(200, T);
-    sphere = new THREE.Mesh(sph, new THREE.MeshBasicMaterial({ vertexColors: THREE.FaceColors }));
+        this.scene = new THREE.Scene();
+        this.sph = new THREE.IcosahedronGeometry(200, this.detalisationLevel);
+        this.sphere = new THREE.Mesh(sph, new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors}));
 
-    let helper = sphere.clone();
-    helper.material = new THREE.MeshBasicMaterial({ wireframe: true, color: 0 });
-    helper.scale.multiplyScalar(1.005);
-    scene.add(sphere);
-    scene.add(helper);
+        let helper = this.sphere.clone();
+        helper.material = new THREE.MeshBasicMaterial({wireframe: true, color: 0});
+        helper.scale.multiplyScalar(1.005);
+        this.scene.add(this.sphere);
+        this.scene.add(helper);
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0x777777);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.sortObjects = false;
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setClearColor(0x777777);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.sortObjects = false;
 
-    container.appendChild(renderer.domElement);
-}
+        this.container.appendChild(this.renderer.domElement);
+    }
 
-export function animate() {
-    requestAnimationFrame(animate);
+    animate() {
+        let self = this;
 
-    ind = parseInt(Date.now() / 1000) % parseInt(20 * Math.pow(4, T));
+        (function loop() {
+            requestAnimationFrame(loop);
+            self.render();
+        })();
+    }
 
-    for ( let i in sph.faces )
-        sph.faces[i].color.setHex(0x555555);
+    render() {
+        let ind = parseInt(Date.now() / 1000) % parseInt(20 * Math.pow(4, this.detalisationLevel));
 
-    sph.faces[ind].color.setHex(0x00ffff);
+        for (let face of this.sph.faces)
+            face.color.setHex(0x555555);
 
-    for ( let i of cells[ind].links )
-        sph.faces[i].color.setHex(0x00ffff);
+        this.sph.faces[ind].color.setHex(0x00ffff);
 
-    sph.elementsNeedUpdate = true;
-    sph.colorsNeedUpdate = true;
-    sph.verticesNeedUpdate = true;
+        for (let i of this.cells[ind].links)
+            this.sph.faces[i].color.setHex(0x00ffff);
 
-    camera.position.x = -Math.sin(mouseX * Math.PI) * 500 * Math.cos(mouseY * Math.PI / 2);
-    camera.position.z = Math.cos(mouseX * Math.PI) * 500 * Math.cos(mouseY * Math.PI / 2);
-    camera.position.y = Math.sin(mouseY * Math.PI / 2) * 500;
+        this.sph.elementsNeedUpdate = true;
+        this.sph.colorsNeedUpdate = true;
+        this.sph.verticesNeedUpdate = true;
 
-    camera.lookAt(scene.position);
+        this.camera.position.x = -Math.sin(mouseX * Math.PI) * 500 * Math.cos(mouseY * Math.PI / 2);
+        this.camera.position.z = Math.cos(mouseX * Math.PI) * 500 * Math.cos(mouseY * Math.PI / 2);
+        this.camera.position.y = Math.sin(mouseY * Math.PI / 2) * 500;
 
-    renderer.render(scene, camera);
-}
+        this.camera.lookAt(this.scene.position);
+
+        this.renderer.render(this.scene, this.camera);
+    }
+};
